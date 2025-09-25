e98a70fe225b67f33e4430ee75eb3809dc04b220
sudrien <_+gitlab@sudrien.net>
2025-09-24 22:10:13 -0400

    fix alignment

--- main.c
+++ main.c
@@ -60,26 +60,51 @@ int main()
     // Create a screen
     lv_obj_t *screen = lv_obj_create(NULL);
 
+    //filename label
+    lv_obj_t *filename = lv_label_create(screen);
+    lv_label_set_text(filename, "filename goes here");
+    lv_obj_align(filename, LV_ALIGN_TOP_LEFT, 0, 0);
+
     // The textarea
     lv_obj_t *input = lv_textarea_create(screen);
-    lv_obj_set_width(input, 280);
-    lv_obj_set_height(input, LV_SIZE_CONTENT);    /// 20
-    lv_obj_center(input);
-    // Enable keyboard input for the text box
-    lv_textarea_set_placeholder_text(input, "Input:");
-    lv_textarea_set_one_line(input, true);
-    lv_obj_set_style_anim_time(input, 5000, LV_PART_CURSOR|LV_STATE_FOCUSED);
+    lv_obj_set_width(input, 320);
+    lv_obj_set_height(input, 250);    /// 20
+    lv_obj_align(input, LV_ALIGN_TOP_LEFT, 0, 20);
+    lv_obj_set_style_border_width(input, 0, 0);
+    lv_obj_set_style_border_width(input, 0, LV_STATE_FOCUSED);
+    lv_obj_set_style_anim_time(input, 1000, LV_PART_CURSOR|LV_STATE_FOCUSED);
     // Textarea event handler
     lv_obj_add_event_cb(input, textarea_event_handler, LV_EVENT_READY, input);
 
-    // Load the screen
-    lv_scr_load(screen);
 
-    // Main loop
-    while (1)
-    {
-        lv_timer_handler();
-        lv_tick_inc(20); // Increment LVGL tick by 5 milliseconds
-        sleep_ms(5); // Sleep for 5 milliseconds}
+    lv_obj_t *f_key_container = lv_obj_create(screen);
+    lv_obj_set_size(f_key_container, 320, 50);
+    lv_obj_align(f_key_container, LV_ALIGN_TOP_LEFT, 0, 270);
+    lv_obj_set_flex_flow(f_key_container, LV_FLEX_FLOW_ROW);
+    lv_obj_set_flex_align(f_key_container, LV_FLEX_ALIGN_SPACE_EVENLY, LV_FLEX_ALIGN_CENTER, LV_FLEX_ALIGN_CENTER);
+
+    // Button labels
+    const char* labels[] = {"F1", "F2", "F3", "F4", "F5"};
+
+    // Create 5 buttons
+    for(int i = 0; i < 5; i++) {
+        lv_obj_t *btn = lv_btn_create(f_key_container);
+        lv_obj_set_size(btn, 55, 40);
+        
+        lv_obj_t *label = lv_label_create(btn);
+        lv_label_set_text(label, labels[i]);
+        lv_obj_center(label);
     }
+
+
+   // Load the screen
+   lv_scr_load(screen);
+
+   // Main loop
+   while (1)
+   {
+       lv_timer_handler();
+       lv_tick_inc(20); // Increment LVGL tick by 5 milliseconds
+       sleep_ms(5); // Sleep for 5 milliseconds}
+   }
 }
